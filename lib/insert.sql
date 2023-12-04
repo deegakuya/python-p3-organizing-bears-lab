a1c0ddfd2d1fd959f606355aa483d19893d0abe9
Diana Gakuya <deegakuya@gmail.com>
2023-12-04 18:11:58 +0300

    sql lab

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+-- Inserting 8 bears
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+  ('Tabitha', 4, 'F', 'White', 'Calm', 1),
+  ('Sergeant Brown', 7, 'M', 'Brown', 'Strict', 1),
+  ('Melissa', 6, 'F', 'Honey', 'Sweet', 1),
+  ('Grinch', 2, 'M', 'Green', 'Mischievous', 1),
+  ('Wendy', 8, 'F', 'Chestnut', 'Gentle', 1),
+  (NULL, NULL, NULL, NULL, NULL, NULL);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,46 @@
+# Query 1
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE sex = 'F';
 """
 
+# Query 2
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT
+        name
+    FROM bears
+    ORDER BY name;
 """
 
+# Query 3
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE alive = 1
+    ORDER BY age;
 """
 
+# Query 4
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age DESC
+    LIMIT 1;
 """
+
+# Query 5
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age
+    LIMIT 1;
 """
